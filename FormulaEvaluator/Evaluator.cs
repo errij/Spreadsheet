cfcc436a1012d1947ccdd52fa4f0a43759c8ce23
민서 김 <errij15@gmail.com>
2024-01-15 22:06:10 -0700

    #첫번째 빌드

--- FormulaEvaluator/Evaluator.cs
+++ FormulaEvaluator/Evaluator.cs
@@ -1,7 +1,39 @@
-﻿namespace FormulaEvaluator
+﻿using System.Numerics;
+using System.Text.RegularExpressions;
+
+namespace FormulaEvaluator
 {
     public class Evaluator
     {
+        public delegate int Lookup(String var);
 
+        public static int Evaluate(String expression, Lookup variableEvaluator)
+        {
+            string[] substrings = Regex.Split(expression, "(\\()|(\\))|(-)|(\\+)|(\\*)|(/)");
+
+            var OperStack = new Stack<String>(); //Operation stack
+            var ValStack = new Stack<int>(); //Value stack
+
+            for (int i = 0; i < substrings.Length; i++)
+            {
+                if (CheckExpression(substrings[i]))
+                {
+
+                }
+            }
+
+            return ValStack.Pop();
+        }
+
+        private static Boolean CheckExpression(String expression)
+        {
+            if (expression is int)
+            {
+                return true;
+            }
+
+            return false;
+        }
     }
+
 }
